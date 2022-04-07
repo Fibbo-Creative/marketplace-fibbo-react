1100140cfd5c289ff3a92a5747baa66c993a4105
antonicarol <antonicarolgit@gmail.com>
2022-04-07 12:38:26 +0200

    Testing git problems

--- src/components/NftCard.js
+++ src/components/NftCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function NftCard({ item }) {
   return (
     <div key={item.name} className="border-grey border-2 p-3 rounded-md">
-      <div className="text-black">
+      <div className="text-gray-700">
         <img src={item.nft.image} alt={item.name} />
       </div>
       <div>
