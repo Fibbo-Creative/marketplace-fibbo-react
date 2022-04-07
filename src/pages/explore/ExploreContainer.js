5209101c11a1394e22955c6718fbaec6236ddbf1
oriol119 <oriol119@gmail.com>
2022-04-07 18:18:08 +0200

    Pantalla explorar OK

--- src/pages/explore/ExploreContainer.js
+++ src/pages/explore/ExploreContainer.js
@@ -21,19 +21,19 @@ export default function ExploreContainer() {
     <div className="absolute h-7 w-full h-full ">
       <div className="flex w-full h-20 items-center justify-center gap-2 pl-5 md:gap-5 ">
         <div className="flex flex-row gap-2 md:gap-5  ">
-          <select autocomplete="All Items" className="w-40 md:w-80 h-10 flex border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+          <select autocomplete="All Items" className="cursor-pointer w-40 md:w-80 h-10 flex border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
             <option>All items</option>
             <option>Single items</option>
           </select>
-          <select autocomplete="country" className="w-30 md:w-80 h-10 flex border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+          <select autocomplete="country" className="cursor-pointer w-30 md:w-80 h-10 flex border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
             <option>Sort by</option>
             <option>Recently Created</option>
             <option>Oldest</option>
           </select>
         </div>
         <div className="flex flex-row gap-2 md:gap-5 ">
-          <button ><Icon icon="typcn:arrow-minimise" width="40" height="40" color="grey" /></button>
-          <button><Icon icon="typcn:arrow-maximise" width="40" height="40" color="grey" /></button>
+          <button className="hover:-translate-y-1" ><Icon icon="typcn:arrow-minimise" width="40" height="40" color="grey" /></button>
+          <button className="hover:-translate-y-1"><Icon icon="typcn:arrow-maximise" width="40" height="40" color="grey" /></button>
         </div>
       </div>
       <div className="flex flex-wrap gap-5 items-center justify-center ">
