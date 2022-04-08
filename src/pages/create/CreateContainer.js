7292113d778034758280e5dfa28839157cbf99ea
antonicarol <antonicarolgit@gmail.com>
2022-04-08 19:55:01 +0200

    Solved some errors

--- src/pages/create/CreateContainer.js
+++ src/pages/create/CreateContainer.js
@@ -84,11 +84,11 @@ export default function CreateContainer() {
   }, [wallet, connectToWallet]);
   return (
     <div className="flex w-auto justify-center items-center pt-10">
-    <div className="block p-6  rounded-lg shadow-xl bg-white max-w-md">
-      <form >
-        <div className="form-group mb-6">
-        
-        <input class="form-control
+      <div className="block p-6  rounded-lg shadow-xl bg-white max-w-md">
+        <form>
+          <div className="form-group mb-6">
+            <input
+              className="form-control
           block
           w-full
           px-2
@@ -102,58 +102,61 @@ export default function CreateContainer() {
           transition
           ease-in-out
           m-0
-          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="formFileSm" type="file" onChange={(e) => onFileSelected(e)}/>
-        </div>
-        <div className="form-group mb-6">
-          <input type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          id="imageInput"
-          className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="formFileSm"
+              type="file"
+              onChange={(e) => onFileSelected(e)}
+            />
+          </div>
+          <div className="form-group mb-6">
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              id="imageInput"
+              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 
             bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            placeholder="Name"/>
-        </div>
-        <div className="form-group mb-6">
-          <textarea className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding 
+              placeholder="Name"
+            />
+          </div>
+          <div className="form-group mb-6">
+            <textarea
+              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding 
           border border-solid border-gray-300 rounded transition ease-in-out m-0
           focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-          rows="3"
-          placeholder="Description"
-          value={desc}
-          onChange={(e) => setDesc(e.target.value)}
-          id="imageInput"
-          type="text"
-        />
-        </div>
-        <div className="form-group mb-6">
-          <input className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700
+              rows="3"
+              placeholder="Description"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+              id="imageInput"
+              type="text"
+            />
+          </div>
+          <div className="form-group mb-6">
+            <input
+              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700
             bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            value={royalty}
-            onChange={(e) => setRoyalty(e.target.value)}
-            id="imageInput"
-            type="text" 
-            placeholder="Royalties"/>
-        </div>
-      
-        <button onClick={(e) => createNFT(e)} type="submit" className=" w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight
+              value={royalty}
+              onChange={(e) => setRoyalty(e.target.value)}
+              id="imageInput"
+              type="text"
+              placeholder="Royalties"
+            />
+          </div>
+
+          <button
+            onClick={(e) => createNFT(e)}
+            type="submit"
+            className=" w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight
           uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Create NFT</button>
-      </form>
+          active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Create NFT
+          </button>
+        </form>
+      </div>
     </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
   );
 }
--- src/pages/item/ItemPage.js
+++ src/pages/item/ItemPage.js
@@ -18,37 +18,51 @@ export default function ItemPage() {
       });
   }, [tokenId]);
   return (
-    <div className="flex w-full h-full justify-center items-center p-5" >
+    <div className="flex w-full h-full justify-center items-center p-5">
       {tokenInfo && (
         <div className="flex flex-row justify-center" key={tokenInfo.name}>
-            <div className="flex flex-wrap justify-center content-center border-grey border-2 p-2 rounded-md ">
+          <div className="flex flex-wrap justify-center content-center border-grey border-2 p-2 rounded-md ">
             <img src={tokenInfo.image} alt={tokenInfo.name} />
-            </div>
+          </div>
           <div className="flex flex-col p-10 gap-5">
-            <p className="text-3xl"><b>{tokenInfo.name}</b></p>
+            <p className="text-3xl">
+              <b>{tokenInfo.name}</b>
+            </p>
             <p>{tokenInfo.description}</p>
             <p className="flex-row justify-center ">
               <b> Owned By: </b>
-              <medium>{tokenInfo.owner?.substring(0, 4)}...{tokenInfo.owner?.substring(
+              <small>
+                {tokenInfo.owner?.substring(0, 4)}...
+                {tokenInfo.owner?.substring(
                   tokenInfo.owner.length - 3,
-                  tokenInfo.owner.length)}
-                </medium>
+                  tokenInfo.owner.length
+                )}
+              </small>
             </p>
             <div className="flex flex-col justify-center flex-wrap border-grey border-2 p-2 rounded-md gap-3">
-                <p>Current Price</p>
-                <div className="flex flex-row gap-3 ">
-                <p>{tokenInfo.royalty}</p> <p className=" decoration-grey align-text-bottom text-xs" >(${tokenInfo.royalty})</p> {/* cambiar token royality y el valor en € */}
-                </div>
-                <div className="flex flex-row gap-5" >
-                <button type="button" class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+              <p>Current Price</p>
+              <div className="flex flex-row gap-3 ">
+                <p>{tokenInfo.royalty}</p>{" "}
+                <p className=" decoration-grey align-text-bottom text-xs">
+                  (${tokenInfo.royalty})
+                </p>{" "}
+                {/* cambiar token royality y el valor en € */}
+              </div>
+              <div className="flex flex-row gap-5">
+                <button
+                  type="button"
+                  className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                >
                   Buy Now
                 </button>
-                <button type="button" class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+                <button
+                  type="button"
+                  className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                >
                   Make offer
                 </button>
-                </div>
-            </div>     
-                
+              </div>
+            </div>
           </div>
         </div>
       )}
