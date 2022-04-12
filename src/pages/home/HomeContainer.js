098d374b89c0da974fb01fb1c0e2b0b378e080a8
oriol119 <oriol119@gmail.com>
2022-04-12 10:41:08 +0200

    avance responsivo homepage

--- src/pages/home/HomeContainer.js
+++ src/pages/home/HomeContainer.js
@@ -5,17 +5,17 @@ export default function HomeContainer() {
   return <div className="absolute w-full h-full m-0 p-0 b-0 ">
     <section className="flex felx-wrap items-center justify-center align-middle w-full h-full gap-10 py-20 px-10">
       <div className="flex flex-col items-center justify-center w-full h-auto ">
-        <h1 className="flex text-6xl p-10 leading-normal"><b>FIBBO : El Arte de expresar tu Arte</b></h1>
-        <p className="text-xl p-10"> Marketplace enfocado con especial énfasis en artitas y creadores de contenido. </p>
+        <h1 className="flex text-2xl leading-normal sm:text-4xl pb-4 md:pb-7 xl:text-6xl xl:p-10 "><b>FIBBO : El Arte de expresar tu Arte</b></h1>
+        <p className="text-sm sm:text-lg md:text-xl p-0  xl:p-10 text-justify"> Marketplace enfocado con especial énfasis en artitas y creadores de contenido. </p>
         <div className="flex felx-wrap gap-10 p-10">
         <a
-            className="lg:inline hidden ml-5 hover:text-blue-400 hover:font-bold text-primary-1 "
+            className="inline  ml-5 hover:text-blue-400 hover:font-bold text-primary-1 "
             href="/explore"
           > 
           Explore
         </a>
         <a
-            className="lg:inline hidden ml-5 hover:text-blue-400 hover:font-bold text-primary-1 "
+            className="inline ml-5 hover:text-blue-400 hover:font-bold text-primary-1 "
             href="/create"
           > 
           Create
@@ -23,7 +23,7 @@ export default function HomeContainer() {
         </div>
       </div>
       <div className="flex items-center w-full justify-center  ">
-        <img src={FiboIMG} alt="FibboLogo" className="flex w-auto"></img>
+        <img src={FiboIMG} alt="FibboLogo" className="flex w-auto p-10 xl:p-0"></img>
 
       </div>
 
