7d48a74ed30f6699d05b6415dc1dd76778b2b55c
oriol119 <oriol119@gmail.com>
2022-04-14 15:28:40 +0200

    Create Page

--- src/pages/create/CreateContainer.js
+++ src/pages/create/CreateContainer.js
@@ -17,6 +17,11 @@ export default function CreateContainer() {
   const { connectToWallet, wallet } = useAccount();
   const [{ nftContract }] = useContractsContext();
 
+  const selectNFTImg = () => {
+    const inputRef = document.getElementById("inputNFT");
+    inputRef.click();
+  };
+
   const onFileSelected = async (e) => {
     const file = e.target.files[0];
     try {
@@ -25,6 +30,11 @@ export default function CreateContainer() {
       const imgAddedToIPFS = await ipfsClient.add(file, {
         progress: (prog) => console.log(`received: ${prog}`),
       });
+
+      document.getElementById("divTextImgNFT").style.visibility = "hidden";
+      document.getElementById("divImgNFT").style.padding = "0";
+      
+
       setIpfsImageUrl(`https://ipfs.infura.io/ipfs/${imgAddedToIPFS.path}`);
 
       const imgAddedToSanity = await marketplaceApi.post(
@@ -38,6 +48,7 @@ export default function CreateContainer() {
       );
       console.log(imgAddedToSanity);
       setSanityImgUrl(imgAddedToSanity.data);
+
     } catch (error) {
       console.log("Error uploading file: ", error);
     }
@@ -86,96 +97,95 @@ export default function CreateContainer() {
     }
   }, [wallet, connectToWallet]);
   return (
-    <div className="flex mt-[90px] justify-center items-center pt-10">
-      <div className="block p-6  rounded-lg shadow-xl  max-w-md">
-        <form>
-          <div className="form-group mb-6">
-            <input
-              className="form-control
-          block
-          w-full
-          px-2
-          py-1
-          text-sm
-          font-normal
-          text-gray-700
-          bg-white bg-clip-padding
-          border border-solid border-gray-300
-          rounded
-          transition
-          ease-in-out
-          m-0
-          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              id="formFileSm"
-              type="file"
-              onChange={(e) => onFileSelected(e)}
-            />
-          </div>
-          <div className="form-group mb-6">
-            <select
-              type="text"
-              /*  value={}
-              onChange={} */
-              disabled="true"
-              placeholder="Collection"
-              id="collectionInput"
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 
-            bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            >
-              <option value={1}>Default Collection</option>
-              <option value={2}>Other</option>
-            </select>
-          </div>
-          <div className="form-group mb-6">
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              id="imageInput"
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 
-            bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              placeholder="Name"
-            />
-          </div>
+    <div className=" flex-col h-full w-full justify-center items-center pt-20">
+    
+      <form className="">
+        <div className="flex gap-20 block p-20">
 
-          <div className="form-group mb-6">
-            <textarea
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding 
-          border border-solid border-gray-300 rounded transition ease-in-out m-0
-          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              rows="3"
-              placeholder="Description"
-              value={desc}
-              onChange={(e) => setDesc(e.target.value)}
-              id="imageInput"
-              type="text"
-            />
+
+          <div className="">
+            <div id="divImgNFT" tabindex="0" bis_skin_checked="1" onClick={selectNFTImg} className="outline-dashed w-80 h-80  flex-col items-center justify-center cursor-pointer ">
+              
+              <input id="inputNFT" onChange={(e) => onFileSelected(e)} accept="image/*" name="uploadImage" type="file" autocomplete="off" className="hidden "/>
+              <img src={ipfsImageUrl} className=""></img>
+              <div id="divTextImgNFT" className="flex h-full items-center justify-center text-center" bis_skin_checked="1"> Drop files here or browse <br></br> JPG, PNG, BMP, GIF, SVG, Max 15mb. </div>
+            </div>
           </div>
-          <div className="form-group mb-6">
-            <input
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700
-            bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+          
+          
+          <div className="w-80">
+            <div className="form-group mb-6">
+              <select
+                type="text"
+                /*  value={}
+                onChange={} */
+                disabled="true"
+                placeholder="Collection"
+                id="collectionInput"
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 
+              bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              >
+                <option value={1}>Default Collection</option>
+                <option value={2}>Other</option>
+              </select>
+            </div>
+            <div className="form-group mb-6">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id="imageInput"
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 
+              bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                placeholder="Name"
+              />
+            </div>
+
+            <div className="form-group mb-6">
+              <textarea
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding 
+            border border-solid border-gray-300 rounded transition ease-in-out m-0
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              value={royalty}
-              onChange={(e) => setRoyalty(e.target.value)}
-              id="imageInput"
-              type="text"
-              placeholder="Royalties"
-            />
-          </div>
-          <button
-            onClick={(e) => createNFT(e)}
-            type="submit"
-            className=" w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight
-          uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            Create NFT
-          </button>
-        </form>
+                rows="3"
+                placeholder="Description"
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+                id="imageInput"
+                type="text"
+              />
+            </div>
+            <div className="form-group mb-6">
+              <input
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700
+              bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0
+              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                value={royalty}
+                onChange={(e) => setRoyalty(e.target.value)}
+                id="imageInput"
+                type="text"
+                placeholder="Royalties"
+              />
+            </div>
+        </div>
       </div>
-    </div>
+
+      <div className="flex justify-center items-center w-full ">
+      <button 
+        onClick={(e) => createNFT(e)}
+        type="submit"
+        className=" w-60 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight
+        uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+        active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+        Create NFT
+      </button>
+      </div>
+      
+    </form>
+  </div>
+    
+  
   );
 }
