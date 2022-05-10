1b0be663e5495ad67c29a9c7d6494a154c0de3df
antonicarol <antonicarolgit@gmail.com>
2022-05-10 20:45:21 +0200

    Done

--- src/pages/profile/ProfileContainer.js
+++ src/pages/profile/ProfileContainer.js
@@ -32,7 +32,7 @@ export default function ProfileContainer() {
   };
 
   const goToNftDetail = (item) => {
-    window.location.reload(
+    window.location.replace(
       `/explore/${item.collectionAddress}/${item.tokenId}`
     );
   };
