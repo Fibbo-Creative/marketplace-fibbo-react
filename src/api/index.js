b362d1672ce4d97dd5140c8748317c73e36770a8
antonicarol <antonicarolgit@gmail.com>
2022-08-01 17:04:02 +0200

    Set up

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const herokuURL = "https://fibbo-market-api.herokuapp.com/";
 const localURL = "http://localhost:9000/";
 const localDevURL = "http://192.168.1.48.sslip.io:9000";
 const herokuDevURL = "https://market-api-dev.herokuapp.com/";
 
-const marketplaceApi = axios.create({ baseURL: herokuURL });
+const marketplaceApi = axios.create({ baseURL: herokuDevURL });
 const isMainnet = false;
 
 export const useApi = () => {
