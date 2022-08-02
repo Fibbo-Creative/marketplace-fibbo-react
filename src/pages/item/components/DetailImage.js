ad90cfd73828c14e81b2204f28e3a5b9af046bc5
oriol119 <oriol119@gmail.com>
2022-08-02 17:00:24 +0200

    Boton QR bien posicionado.

--- src/pages/item/components/DetailImage.js
+++ src/pages/item/components/DetailImage.js
@@ -21,26 +21,26 @@ export default function DetailImage({ tokenImage, tokenName, loading }) {
       else 
         setImgOrQr(tokenImage)
 
-
-      
     })
 
   }
 
   return (
     <div className="col-span-1 flex items-center justify-center dark:bg-dark-2 ">
-      <div className="w-[450px] h-[400px] border-gray border-2 p-2 rounded-md  ">
+      <div className="w-[450px] h-[450px] border-gray border-2 p-2 rounded-md flex flex-col gap-8 items-center ">
         {loading ? (
           <div className="w-full h-full animate-pulse bg-gray-400"></div>
         ) : (
           <img
-            className="object-contain w-full h-full "
+            className="object-contain w-full h-5/6 "
             src={imgOrQR}
             alt={tokenName}
           />
           
         )}
-        <Icon className="flex cursor-pointer" onClick={GenerateQRCode} icon="ion:qr-code" />
+        
+        <Icon className="flex w-[30px] h-[30px] cursor-pointer b-0" onClick={GenerateQRCode} icon="ion:qr-code" /> 
+    
 
       </div>
 
