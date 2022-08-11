40a2dac7f8157e232395656d7efc28aec87d5370
Loana Rodrigues Morais <lrodriguesm@educand.ad>
2022-08-11 15:50:28 +0200

    Visualizar mis colecciones inicio

--- src/pages/collection/MyCollectionsContainer.js
+++ src/pages/collection/MyCollectionsContainer.js
@@ -21,6 +21,67 @@ export default function MyCollectionsContainer() {
             <div className="flex w-full content-center justify-center p-[40px]">
                 <ActionButton text="Crear Colección" size="large" buttonAction={() => goToCreateCollection()} />
             </div>
+            <div className="flex flex-row flex-wrap gap-20 w-full content-center justify-center p-[40px]">
+                <div className="flex flex-col w-[400px] h-[300px] border-4 border-white">
+                    <div className="flex w-full h-[200px] items-center justify-center border-b-4 border-white ">
+                        IMAGEN PRINCIPAL COLECCION
+                    </div>
+                    <div className="flex w-full h-[100px]">
+                        <div className="flex w-[100px] h-full border-r-4 border-white items-center justify-center ">
+                            LOGO
+                        </div>
+                        <div className="flex w-[300px] h-full items-center justify-center ">
+                            Titulo Coleccion
+                        </div>
+                    </div> 
+                </div>
+
+                <div className="flex flex-col w-[400px] h-[300px] border-4 border-white">
+                    <div className="flex w-full h-[200px] items-center justify-center border-b-4 border-white ">
+                        IMAGEN PRINCIPAL COLECCION
+                    </div>
+                    <div className="flex w-full h-[100px]">
+                        <div className="flex w-[100px] h-full border-r-4 border-white items-center justify-center ">
+                            LOGO
+                        </div>
+                        <div className="flex w-[300px] h-full items-center justify-center ">
+                            Titulo Coleccion
+                        </div>
+                    </div> 
+                </div>
+
+                <div className="flex flex-col w-[400px] h-[300px] border-4 border-white">
+                    <div className="flex w-full h-[200px] items-center justify-center border-b-4 border-white ">
+                        IMAGEN PRINCIPAL COLECCION
+                    </div>
+                    <div className="flex w-full h-[100px]">
+                        <div className="flex w-[100px] h-full border-r-4 border-white items-center justify-center ">
+                            LOGO
+                        </div>
+                        <div className="flex w-[300px] h-full items-center justify-center ">
+                            Titulo Coleccion
+                        </div>
+                    </div> 
+                </div>
+
+                <div className="flex flex-col w-[400px] h-[300px] border-4 border-white">
+                    <div className="flex w-full h-[200px] items-center justify-center border-b-4 border-white ">
+                        IMAGEN PRINCIPAL COLECCION
+                    </div>
+                    <div className="flex w-full h-[100px]">
+                        <div className="flex w-[100px] h-full border-r-4 border-white items-center justify-center ">
+                            LOGO
+                        </div>
+                        <div className="flex w-[300px] h-full items-center justify-center ">
+                            Titulo Coleccion
+                        </div>
+                    </div> 
+                </div>
+                
+
+
+
+            </div>
 
         </div>
     )
