f61fb8941912210c69e47e60f390c33425d3971b
antonicarol <antonicarolgit@gmail.com>
2022-09-12 17:43:06 +0200

    Prepared

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -11,7 +11,7 @@ export const Contracts = {
       "https://api.defender.openzeppelin.com/autotasks/916b92b5-3b76-4c4a-80df-1abf8a29e65e/runs/webhook/3185bde5-abcb-4ba8-90ca-3f40635cca92/36Q8d9V6MWW8zeySQ2rX7h",
   },
   [ChainId.FANTOM_TESTNET]: {
-    addressRegistry: "0x04661ABcD31E73064BA662549aD2A3e6642f7886",
+    addressRegistry: "0x3AB3e6D66a2f4688c5746F836539a1dDa6D191eB",
     minimalForwarder: "0x7687D1fcF56779b19beCBFae93cabAD1EA77878b",
     wftmAddress: "0x4EEf747dC4f5d110d9bCfA5C6F24b3359bD4B2d4",
     autotaskURL:
