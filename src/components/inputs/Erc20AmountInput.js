93fc268f9ec7a6dac983517e166eff6055b1dbf8
antonicarol <antonicarolgit@gmail.com>
2022-09-15 11:35:40 +0200

    Cleaned logs

--- src/components/inputs/Erc20AmountInput.js
+++ src/components/inputs/Erc20AmountInput.js
@@ -16,6 +16,7 @@ export const Erc20AmountInput = ({
   selectDisabled,
   setSelectedToken = true,
   showBalance,
+  nonSelect,
 }) => {
   const { getAllPayTokens } = useApi();
   const { getWFTMBalance } = useWFTMContract();
