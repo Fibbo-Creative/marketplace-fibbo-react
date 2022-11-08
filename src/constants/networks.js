0bebcb15111d8c31e37d0a690cb4c803e905c158
antonicarol <antonicarolgit@gmail.com>
2022-11-08 16:02:55 +0100

    Prepared new branch

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -11,7 +11,7 @@ export const Contracts = {
       "https://api.defender.openzeppelin.com/autotasks/916b92b5-3b76-4c4a-80df-1abf8a29e65e/runs/webhook/3185bde5-abcb-4ba8-90ca-3f40635cca92/36Q8d9V6MWW8zeySQ2rX7h",
   },
   [ChainId.FANTOM_TESTNET]: {
-    addressRegistry: "0x02c744763651E4a7Bd079c04d5EAA6e18a46E00d",
+    addressRegistry: "0x3C70Ff5af18C8462ed32724e51213c2E6BC4e3f0",
     minimalForwarder: "0x944f2A65742dbe3963BeDeE9d55cdF02572db6Da",
     wftmAddress: "0x2b513ea648453a3Cf6EF3A801356d54A2dB625e9",
     autotaskURL:
