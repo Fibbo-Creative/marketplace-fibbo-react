e8da5075d6e32b17f39f3b1064d7e9a6d2438120
oriol119 <oriol119@gmail.com>
2022-12-28 13:02:36 +0100

    faq v.1.0.

--- src/pages/faq/FaqContainer.js
+++ src/pages/faq/FaqContainer.js
@@ -18,7 +18,13 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q1}>
-            </DropDown>
+            
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a1_1}</p>
+                <p className="p-2 text-justify">{literals.faq.a1_2}</p>
+              </div>
+
+          </DropDown>
          </div>
 
          <div className="flex mt-4 w-full items-center justify-center">
@@ -27,6 +33,12 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q2}>
+
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a2_1}</p>
+                <p className="p-2 text-justify">{literals.faq.a2_2}</p> <a>{literals.faq.a2_3}</a>
+              </div>
+
             </DropDown>
          </div>
 
@@ -36,6 +48,19 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q3}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a3_1}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_2}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_3}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_4}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_5}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_6}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_7}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_8}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_9}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_10}</p>
+                <p className="p-2 text-justify">{literals.faq.a3_11}</p>
+              </div>
             </DropDown>
          </div>
 
@@ -45,6 +70,12 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q4}>
+
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a4_1}</p>
+                <p className="p-2 text-justify">{literals.faq.a4_2}</p> 
+                <p className="p-2 text-justify">{literals.faq.a4_3}</p> 
+              </div>
             </DropDown>
          </div>
 
@@ -54,6 +85,10 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q5}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a5_1}</p>
+                <p className="p-2 text-justify">{literals.faq.a5_2}</p> 
+              </div>
             </DropDown>
          </div>
 
@@ -63,6 +98,10 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q6}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a6_1}</p>
+                <p className="p-2 text-justify">{literals.faq.a6_2}</p> 
+              </div>
             </DropDown>
          </div>
 
@@ -72,6 +111,9 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q7}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a7_1}</p>
+              </div>
             </DropDown>
          </div>
 
@@ -81,6 +123,9 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q8}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a8}</p>
+              </div>
             </DropDown>
          </div>
 
@@ -90,6 +135,11 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q9}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a9_1}</p>
+                <p className="p-2 text-justify">{literals.faq.a9_2}</p>
+
+              </div>
             </DropDown>
          </div>
 
@@ -99,6 +149,9 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q10}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a10}</p>
+              </div>
             </DropDown>
          </div>
 
@@ -108,6 +161,12 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q11}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a11_1}</p>
+                <p className="p-2 text-justify">{literals.faq.a11_2}</p>
+                <p className="p-2 text-justify">{literals.faq.a11_3}</p>
+
+              </div>
             </DropDown>
          </div>
 
@@ -117,6 +176,9 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q12}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-text-justify">{literals.faq.a12}</p>
+              </div>
             </DropDown>
          </div>
 
@@ -126,6 +188,9 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q13}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a13}</p>
+              </div>
             </DropDown>
          </div>
 
@@ -135,6 +200,9 @@ export default function FaqContainer() {
             className={`mb-5 dark:bg-dark-2 text-white w-4/5 xs:w-[500px] md:w-[650px] lg:w-[900px] text-bold`}
             icon="material-symbols:arrow-drop-down"
             title={literals.faq.q14}>
+              <div className="flex flex-col Class Properties text-left">
+                <p className="p-2 text-justify">{literals.faq.a14}</p>
+              </div>
             </DropDown>
          </div>
        
